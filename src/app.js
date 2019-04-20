0a9193e6b54854b969f393ac520dacfd714315e2
gangavath <stackmern@gmail.com>
2019-04-20 23:31:26 +0530

    Commit message

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import  ReactDOM from 'react-dom'
+
+const APP = () => {
+    return (
+        <div>
+            <h1>
+                coole
+            </h1>
+        </div>
+    );
+};
+ReactDOM.render('<APP', document.getElementById("app"))
